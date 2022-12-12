b87fe49c7ded12c11a7ee73f5224affb01fa2a9d
Caleb Baum <lorencalebbaum@gmail.com>
2022-12-12 11:09:41 -0500

    start 12th

---
+++ src/twelfth-day.rs
@@ -0,0 +1,8 @@
+use itertools::Itertools;
+
+// const PROMPT_STR: &str = include_str!("../test_data/twelfth_day.txt");
+const TEST_DATA: &str = include_str!("../test_data/test.txt");
+
+fn main() {
+
+}
