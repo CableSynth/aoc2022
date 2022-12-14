b60c35a4ffb01289f454c547d79eb39d93b5643d
Caleb Baum <lorencalebbaum@gmail.com>
2022-12-14 16:22:36 -0500

    worked on the parsing

--- src/thirteenth-day.rs
+++ src/thirteenth-day.rs
@@ -3,14 +3,22 @@ use itertools::Itertools;
 const INPUT: &str = include_str!("../test_data/day-13.txt");
 const TEST_INPUT: &str = include_str!("../test_data/test.txt");
 
+fn parse_pair(pair:&str) -> bool {
+    let left_right = pair.lines().map(|line| line.chars().collect_vec()).collect_vec();
+    let mut left = left_right[0].clone();
+    println!("{:?}", left);
+    let mut right = left_right[1].clone();
+    println!("{:?}", right);
+    let mut parantheses = 0;
+    for (l, r) in left.iter().zip(left.iter())) {
+        if  {
+            return true;
+        }
+    }
+    true
+}
 
 fn main() {
-    let input = TEST_INPUT.split("\n\n").map(|x| x.split("\n").map(|line| line.split_terminator(&['[',']'][..]).filter_map(|s| {
-        if s.is_empty() || s == "," {
-            None
-        } else {
-            Some(s)
-        }
-    }).collect_vec()).collect_vec()).collect_vec();
-    println!("{:?}", input);
+    let input = TEST_INPUT.split("\n\n").collect_vec();
+    let result = input.iter().map(|pair| parse_pair(pair)).collect_vec();
 }
